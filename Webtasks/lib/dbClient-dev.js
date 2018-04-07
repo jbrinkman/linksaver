13c6ac7eb5b837346a9bb85efda527dcb05970f4
Joe Brinkman <joe@dnncorp.com>
2018-04-07 06:12:03 -0700

    Save links in log file for development

--- Webtasks/lib/dbClient-dev.js
+++ Webtasks/lib/dbClient-dev.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 
 dbClient.prototype.insertDocument = function (doc) {
-    //TODO: Add code to append this to a local text file
-    
-    console.log(`Document: ${doc}`);
-}; 
+    fs.appendFile(require('app-root-path').resolve(this.dbfile), JSON.stringify(doc) + '\n', 'utf8',
+       function (err) {
+            if (err) throw err;
+            // if no error
+            console.log("Link is appended to file successfully.")
+        });
+};
 
 function dbClient(dbconfig) {
 
@@ -13,6 +17,7 @@ function dbClient(dbconfig) {
 
     this.config = dbconfig;
     this.isDebug = true;
+    this.dbfile = `/data/${this.config.databaseId}.${this.config.collectionId}.json`;
 }
 
-module.exports = dbClient; 
+module.exports = dbClient;
